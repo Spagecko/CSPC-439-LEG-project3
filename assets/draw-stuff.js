d0447dcdc5ee70600cde8ca302a60b4e42b8ac05
Spagecko <bennettlawrenz@gmail.com>
2019-04-27 12:13:17 -0700

    Add files via upload

--- assets/draw-stuff.js
+++ assets/draw-stuff.js
@@ -25,7 +25,185 @@ function test(ctx)
 	createArray(); 
 	drawArray(ctx, arry); 
 }
+function draw_BIG_cell (ctx, x, y)
+{
+	var stroke = 'transparent';
+	var  fill = 'Green';
+	ctx.save();
+	ctx.strokeStyle = stroke;
+	ctx.fillStyle = fill;
+	ctx.lineWidth = 0;
+	var gen = 0;
+	width = canvas.width - 210;
+	ctx.rect(x,y,17,17);
+	ctx.fill();
+	//ctx.restore();
+}
+function re_BIG_cell (ctx, x, y)
+{
+	var stroke = 'transparent';
+	var  fill = 'black';
+	ctx.save();
+	ctx.strokeStyle = stroke;
+	ctx.fillStyle = fill;
+	ctx.lineWidth = 0;
+	var gen = 0;
+	width = canvas.width - 210;
+	ctx.rect(x,y,17,17);
+	ctx.fill();
+	//ctx.restore();
+}
 
+function move_turing_cell(ctx, x, y) {
+    var stroke = 'transparent';
+    var fill = 'yellow';
+    ctx.save();
+    ctx.strokeStyle = stroke;
+    ctx.fillStyle = fill;
+    ctx.lineWidth = 0;
+    var gen = 0;
+    width = canvas.width - 210;
+    ctx.fillRect(x, y, 8.5, 8.5);
+    ctx.restore();
+}
+
+function read_turing_cell(ctx, x, y) {
+    var stroke = 'transparent';
+    var fill = 'blue';
+    ctx.save();
+    ctx.strokeStyle = stroke;
+    ctx.fillStyle = fill;
+    ctx.lineWidth = 0;
+    var gen = 0;
+    width = canvas.width - 210;
+    ctx.fillRect(x, y, 8.5, 8.5);
+    ctx.restore();
+}
+
+function write_turing_cell(ctx, x, y) {
+    var stroke = 'transparent';
+    var fill = 'green';
+    ctx.save();
+    ctx.strokeStyle = stroke;
+    ctx.fillStyle = fill;
+    ctx.lineWidth = 0;
+    var gen = 0;
+    width = canvas.width - 210;
+    ctx.fillRect(x, y, 8.5, 8.5);
+    ctx.restore();
+}
+
+function clear_small_cell(ctx, x, y) {
+    ctx.clearRect(x, y, 8.5, 8.5);
+    ctx.restore();
+}
+function NoteStateMachineReadRight(ctx, ctx4)
+{
+        
+        globalX += 10;
+        read_turing_cell(ctx4, globalX, globalY);
+        currentState++;
+        tmpArrSet[0] = DrawArr[i - 1];
+	    draw_BIG_cell(ctx, 50,50); 
+}
+
+function NoteStateMachineReadMiddle(ctx, ctx4)
+{
+        globalX += 10;
+        read_turing_cell(ctx4, globalX, globalY);
+        currentState++;
+        tmpArrSet[1] = DrawArr[i];
+        draw_BIG_cell(ctx, 50,50);
+	
+}
+
+function NoteStateMachineReadLeft(ctx, ctx4)
+{
+        globalX -= 10;
+        read_turing_cell(ctx4, globalX, globalY);
+        currentState++;
+        tmpArrSet[2] = DrawArr[i + 1];
+		draw_BIG_cell(ctx, 50,50); 
+}
+
+function NoteStateMachineWrite(ctx)
+{
+		draw_BIG_cell(ctx, 300,120); 
+}
+
+function NoteStateMachineMove(ctx, ctx4)
+{
+        i++;
+        currentState++;
+        globalX += 10;
+        move_turing_cell(ctx4, globalX, globalY);
+		draw_BIG_cell(ctx,50,50); 
+}
+
+function NoteStateMachineWriteCellR(ctx)
+{
+		draw_BIG_cell(ctx, 300,30); 
+}
+function NoteStateMachineWriteCellL(ctx)
+{
+		draw_BIG_cell(ctx, 100,30); 
+}
+function NoteStateMachineWriteCellM(ctx,ctx3,ctx4)
+{
+        globalY += 10;
+        write_turing_cell(ctx4, globalX, globalY);
+        draw_cell(ctx,globalX, globalY );
+        globalY -= 10;
+        currentState = 0;
+		draw_BIG_cell(ctx3, 50,30); 
+}
+
+
+/// reset functions  
+
+
+function reNoteStateMachineReadRight(ctx)
+{
+	re_BIG_cell(ctx, 50,50); 
+}
+
+function reNoteStateMachineReadMiddle(ctx)
+{
+ 
+	re_BIG_cell(ctx, 50,50);
+}
+
+function reNoteStateMachineReadLeft(ctx)
+{
+		re_BIG_cell(ctx, 50,50); 
+}
+
+function reNoteStateMachineWrite(ctx)
+{
+		re_BIG_cell(ctx, 50,50); 
+}
+
+function reNoteStateMachineMove(ctx)
+{
+		re_BIG_cell(ctx, 50,50); 
+}
+
+function reNoteStateMachineWriteCellR(ctx)
+{
+		re_BIG_cell(ctx, 300,30); 
+}
+function reNoteStateMachineWriteCellL(ctx)
+{
+		re_BIG_cell(ctx, 100,30); 
+}
+function reNoteStateMachineWriteCellM(ctx)
+{
+		re_BIG_cell(ctx, 50,30); 
+}
+function ClearNote(ctx )
+{
+	// reset graph 
+}
 function draw_rect( ctx, stroke, fill ) 
 {
     stroke = stroke || 'lightgrey';
